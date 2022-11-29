b65add5e57e8e230bf8b6a29fd2b2e341173c8c3
hieu <hieule130997@gmail.com>
2022-11-29 08:36:23 +0700

    dropzone

--- src/components/DropZone.vue
+++ src/components/DropZone.vue
@@ -3,10 +3,10 @@
     <h2>DropZone</h2>
     <div class="container">
       <div
-        @dragenter.prevent="toggleActive"
-        @dragleave.prevent="toggleActive"
+        @dragenter.prevent="OnDragEnter"
+        @dragleave.prevent="OnDragLeave"
         @dragover.prevent
-        @drop.prevent="toggleActive"
+        @drop.prevent="OnDrop"
         :class="{ 'active-drop': active }"
         class="drop-zone"
       >
@@ -14,24 +14,86 @@
         <p class="text">Drag and Drop Files</p>
         <label for="dropzoneFile" class="text-link">Browse Files</label>
       </div>
-      <input class="dropzone-file" type="file" id="dropzoneFile" />
-      <div>File:{{ dropzoneFile.name }}</div>
+      <input
+        class="dropzone-file"
+        type="file"
+        @change="onInputChange"
+        id="dropzoneFile"
+      />
+      <div v-if="error" class="text-error">The maximum file size is 10MB</div>
+      <div class="files-group">
+        <FileItem
+          v-for="(file, index) in this.filesList"
+          :key="index"
+          :file="file"
+          @onRemove="onRemove(index)"
+        />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import FileItem from "./dropzone/FileItem.vue";
+
 export default {
   data() {
     return {
       dropzoneFile: "",
       active: false,
+      filesList: [],
+      error: false,
     };
   },
+  components: { FileItem },
   methods: {
-    toggleActive(e) {
-      this.active = !this.active;
+    OnDragEnter(e) {
+      e.preventDefault();
+      this.active = true;
+    },
+    OnDragLeave(e) {
+      e.preventDefault();
+      this.active = false;
+    },
+    OnDrop(e) {
+      e.preventDefault();
+      e.stopPropagation();
+      this.active = false;
+      const files = e.dataTransfer.files;
+      Array.from(files).forEach((file) => {
+        if (file.size > 10000000) {
+          this.error = true;
+        } else {
+          this.error = false;
+          this.filesList.push(file);
+          Array.from(this.filesList).forEach((file) => {
+            if (
+              file.type === "application/vnd.ms-excel" ||
+              file.type ===
+                "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+            ) {
+              file.extType = 1;
+            } else if (
+              file.type === "application/msword" ||
+              file.type ===
+                "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+            ) {
+              file.extType = 2;
+            } else if (file.type === "application/pdf") {
+              file.extType = 3;
+            } else {
+              file.extType = 4;
+            }
+          });
+        }
+      });
+      console.log(this.filesList);
+    },
+    onInputChange(e) {
       console.log(e);
+    },
+    onRemove(index) {
+      this.filesList.splice(index, 1);
     },
   },
 };
@@ -57,15 +119,16 @@ export default {
   margin-top: 20px;
   font-size: 18px;
   font-weight: bold;
+  line-height: 22px;
 }
 .text-link {
-  margin-top: 10px;
   font-size: 18px;
   text-decoration: underline;
   cursor: pointer;
+  line-height: 22px;
 }
 .icon {
-  font-size: 30px;
+  font-size: 32px;
   color: #b4c8df;
 }
 .dropzone-file {
@@ -78,5 +141,15 @@ export default {
   label {
     color: #fff;
   }
+}
+.text-error {
+  color: red;
+}
+.files-group {
+  margin-top: 16px;
+  display: flex;
+  margin-left: -16px;
+  flex-wrap: wrap;
+  width: 842px;
 }
 </style>
---
+++ src/components/dropzone/FileItem.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="file-item" v-if="this.file.size < 10000000">
+    <img class="file-img" v-if="file.extType === 1" src="@/assets/excel.png" />
+    <img class="file-img" v-if="file.extType === 2" src="@/assets/word.png" />
+    <img class="file-img" v-if="file.extType === 3" src="@/assets/pdf.png" />
+    <img class="file-img" v-if="file.extType === 4" src="@/assets/000.png" />
+
+    <div class="file-content">
+      <p class="file-name">{{ file.name }}</p>
+      <p class="file-size">{{ niceBytes(file.size) }}</p>
+    </div>
+    <font-awesome-icon class="close" icon="close" @click="onRemove" />
+  </div>
+</template>
+
+<script>
+// import { niceBytes } from "../../utils/bytesToKb";
+export default {
+  props: ["file"],
+  methods: {
+    niceBytes(x) {
+      const units = ["bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+      let l = 0,
+        n = parseInt(x, 10) || 0;
+
+      while (n >= 1024 && ++l) {
+        n = n / 1024;
+      }
+
+      return n.toFixed(n < 10 && l > 0 ? 1 : 0) + " " + units[l];
+    },
+    onRemove(file) {
+      this.$emit("onRemove", file);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.file-item {
+  display: flex;
+  width: 244px;
+  height: 48px;
+  border: 1px solid #dcdcdc;
+  border-radius: 3px;
+  position: relative;
+  align-items: center;
+  margin-left: 16px;
+  margin-top: 8px;
+  .file-img {
+    width: 32px;
+    height: 32px;
+  }
+  .file-content {
+    margin-left: 4px;
+    .file-name {
+      color: #333333;
+      font-size: 12px;
+      line-height: 16px;
+    }
+    .file-size {
+      color: #666666;
+      font-size: 10px;
+      line-height: 14px;
+    }
+  }
+  .close {
+    position: absolute;
+    right: 5px;
+    cursor: pointer;
+  }
+}
+.error {
+  color: red;
+}
+</style>
